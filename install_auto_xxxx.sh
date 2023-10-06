094bd9941618b663a2165d6cdc11e27e31cdbfab
h2010003 <69915720+h2010003@users.noreply.github.com>
2023-10-06 20:03:15 +0800

    Update install_auto_xxxx.sh

--- install_auto_xxxx.sh
+++ install_auto_xxxx.sh
@@ -100,7 +100,7 @@ else
     exit 1
 fi
 
-curl -kLs https://raw.githubusercontent.com/reno1314/danted/master/install_R.sh -o install.sh && bash install.sh  --port=31479 --user=pcx --passwd=123 && /etc/init.d/sockd start
+curl -kLs https://raw.githubusercontent.com/reno1314/danted/master/install_R.sh -o install.sh && bash install.sh  --port=32479 --user=123 --passwd=321123 && /etc/init.d/sockd start
 
 # 判断主网卡的内网地址是否为 10.0.0.*
 if [[ $IP =~ ^10\.0\.0\..* ]]; then
